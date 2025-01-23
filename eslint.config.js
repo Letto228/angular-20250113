fe802ba8cfc3ba5811238cc3064de860ff2f329b
Dmitry Starovoytov <>
2025-01-23 18:16:21 +0300

    updated eslint and prettier configurations

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,24 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const google = require('eslint-config-google');
+const prettierConfig = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = tseslint.config(
     {
         files: ['**/*.ts'],
         extends: [
+            google,
+            prettierConfig,
             eslint.configs.recommended,
             ...tseslint.configs.recommended,
             ...tseslint.configs.stylistic,
             ...angular.configs.tsRecommended,
         ],
+        plugins: {
+            prettier: prettierPlugin,
+        },
         processor: angular.processInlineTemplates,
         rules: {
             '@angular-eslint/directive-selector': [
