e8358ecd569097d697cc9b9509d6a6651d80e56b
Letto228 <letto1101@gmail.com>
2025-01-24 00:21:00 +0300

    chore: sanitize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
 
 @Component({
     selector: 'app-root',
-    // selector: 'div#root',
     standalone: true,
     imports: [HeaderComponent],
     templateUrl: './app.component.html',
-    // template: '<h1>Hello</h1>',
     styleUrl: './app.component.css',
-    // styleUrls: ['./app.component.css'],
-    // styles: [''],
-    encapsulation: ViewEncapsulation.Emulated,
 })
-export class AppComponent {
-    title = 'angular-20250113';
-}
+export class AppComponent {}
