b256e26b3c4c11ba79aa79202685d79b79ab78cb
Arthur Buravlev <buravlev-arthur@yandex.ru>
2025-01-26 19:16:22 +0300

    Homework 1: set eslint, prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -30,11 +30,42 @@ module.exports = tseslint.config(
                     style: 'kebab-case',
                 },
             ],
+            '@angular-eslint/sort-lifecycle-methods': ['error'], // требует определения хуков в порядке их выполнения
+            '@angular-eslint/component-max-inline-declarations': [
+                // определяет количество строк для inline-стилей, шаблона и анимаций
+                'error',
+                {
+                    template: 12,
+                    styles: 24,
+                    animations: 36,
+                },
+            ],
+            '@angular-eslint/no-async-lifecycle-method': ['error'], // запрещает использовать асинхронные куки
+            '@angular-eslint/consistent-component-styles': ['error', 'string'], // запрещает использовать массивы для стилей компоненты
+            '@angular-eslint/prefer-standalone': ['error'], // требует, чтобы все компоненты, директивы и пайпы были типа standalone
         },
     },
     {
         files: ['**/*.html'],
         extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-        rules: {},
+        rules: {
+            '@angular-eslint/template/attributes-order': [ // порядок атрибутов
+                'error',
+                {
+                    alphabetical: true,
+                    order: [
+                        "STRUCTURAL_DIRECTIVE",
+                        "TEMPLATE_REFERENCE",
+                        "ATTRIBUTE_BINDING",
+                        "INPUT_BINDING",
+                        "TWO_WAY_BINDING",
+                        "OUTPUT_BINDING",
+                    ]
+                }
+            ],
+            '@angular-eslint/template/button-has-type': ['error'], // требует атрибут type у кнопок
+            '@angular-eslint/template/no-any': ['error'], // запрещает использовать $any в шаблонах
+            '@angular-eslint/template/no-inline-styles': ['error'], // запрещает inline-стили в шаблонах
+        },
     },
 );
