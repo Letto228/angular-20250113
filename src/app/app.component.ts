01dbdee03bf0a981f907714756ce52e831da4b94
Anton Lyhin <dotnet@lyhin.com>
2025-01-26 14:32:15 +0300

    prettier semi, after apply;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
     title = 'angular-20250113';
-    checkIfPrettierSemiWorks = 'semi'
+    checkIfPrettierSemiWorks = 'semi';
 }
