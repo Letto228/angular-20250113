a0e4717a095e6fb5207ea39fdb19466119bb5339
Arthur Buravlev <buravlev-arthur@yandex.ru>
2025-02-04 14:23:49 +0300

    feat(popup-host.component): implemented popup-host component

---
+++ src/app/components/popup-host/popup-host.component.ts
@@ -0,0 +1,48 @@
+import {
+    ChangeDetectionStrategy,
+    Component,
+    input,
+    effect,
+    viewChild,
+    ViewContainerRef,
+} from '@angular/core';
+import type {TemplateRef} from '@angular/core';
+
+@Component({
+    selector: 'app-popup-host',
+    standalone: true,
+    imports: [],
+    templateUrl: './popup-host.component.html',
+    styleUrl: './popup-host.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PopupHostComponent {
+    template = input<TemplateRef<Element> | null>();
+    templateContainer = viewChild<ViewContainerRef, ViewContainerRef>('templateContainer', {
+        read: ViewContainerRef,
+    });
+
+    constructor() {
+        effect(() => {
+            const container = this.templateContainer();
+            const template = this.template();
+
+            console.info('template: ', template);
+
+            // если контейнер не найден - до свидания
+            if (!container) {
+                return;
+            }
+
+            // очищаем view контейнера, если он не пустой
+            if (container.length) {
+                container.clear();
+            }
+
+            // если передан новый шаблон - устанавливаем его в контейнер
+            if (template) {
+                container.createEmbeddedView(template);
+            }
+        });
+    }
+}
