db8e39ed26c05629cff2135ff1a59727a1b1914f
Anton Lyhin <dotnet@lyhin.com>
2025-02-06 16:18:51 +0300

    Lecture-2: rating fix; (+3 squashed commit)
    Squashed commit:
    
    [77a5502] Lecture-2: rating;
    
    [919e2ad] Lecture-2: replaced angular style binding with css;
    
    [15e9a3b] Lecture-2: hw task 3 stopPropagation;

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -2,27 +2,45 @@ import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
-import {NgFor} from '@angular/common';
+import {NgFor, NgIf} from '@angular/common';
 import {productsMock} from '../../../shared/products/products.mock';
 
 @Component({
     selector: 'app-card',
     standalone: true,
-    imports: [NgFor, MatCardModule, MatButtonModule, MatIcon],
+    imports: [NgFor, NgIf, MatCardModule, MatButtonModule, MatIcon],
     templateUrl: './card.component.html',
     styleUrl: './card.component.css',
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    readonly product = productsMock[1];
+    readonly ratingValue: number;
+    readonly ratingItems: boolean[];
 
-    buy() {
+    constructor() {
+        this.ratingValue = Math.floor(this.product.rating);
+        this.ratingItems = this.computeRatingItems(this.ratingValue);
+    }
+
+    buy(event: Event) {
+        event.stopPropagation(); // https://stackoverflow.com/a/5963688
+
         /* eslint-disable no-console */
         console.log('Buy product');
     }
 
-    getRating() {
-        const rating = Math.floor(this.product.rating);
+    buyPropagationCard() {
+        /* eslint-disable no-console */
+        console.log('Stop propagation not works.');
+    }
 
-        return [rating, 5 - rating];
+    computeRatingItems(ratingValue: number): boolean[] {
+        const ratingItems: boolean[] = [false, false, false, false, false];
+
+        for (let i = 0; i < ratingValue; i++) {
+            ratingItems[i] = true;
+        }
+
+        return ratingItems;
     }
 }
