541e2551a01f40c9de2a87fc98f8a8de8b43a130
Dmitry S. Denisov <Dmitry.S.Denisov@kaspersky.com>
2025-02-06 22:03:16 +0300

    fix card component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,12 +1,19 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {NgFor} from '@angular/common';
 import {CardComponent} from './card/card.component';
+import {Product} from '../../shared/products/product.interface';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
     standalone: true,
-    imports: [CardComponent],
+    imports: [CardComponent, NgFor],
     templateUrl: './products-list.component.html',
     styleUrl: './products-list.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    get products(): Product[] {
+        return productsMock;
+    }
+}
