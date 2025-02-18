420f95f7021a905d28f0358711b209e7c07f8437
Сергей Шмидт <serge@192.168.1.3>
2025-02-18 17:31:49 +0300

    [dz-7 shmidt] scroll loader

---
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -0,0 +1,32 @@
+import {Directive, input, output} from '@angular/core';
+
+@Directive({
+    selector: '[appScrollWithLoading]',
+    standalone: true,
+    host: {
+        '(scroll)': 'onScroll($event)',
+    },
+})
+export class ScrollWithLoadingDirective {
+    loadNextData = output<void>();
+    borderOffset = input(100);
+    lastScrollTop = 0;
+
+    onScroll(event: Event & {target: HTMLElement}) {
+        if (!event?.target) {
+            return;
+        }
+
+        const {target} = event;
+        const {scrollHeight, scrollTop, clientHeight} = target;
+
+        if (
+            this.lastScrollTop < scrollTop &&
+            scrollHeight - scrollTop - clientHeight <= this.borderOffset()
+        ) {
+            this.loadNextData.emit();
+        }
+
+        this.lastScrollTop = scrollTop;
+    }
+}
