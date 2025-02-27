9dbd97ea74418d6f7043e84597c1231b848ca57e
Dmitry Starovoytov <>
2025-02-27 14:25:48 +0300

    implemented popup service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,16 @@
-import {ChangeDetectionStrategy, Component, input, output, TemplateRef} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    input,
+    output,
+    TemplateRef,
+} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {ApplicationConfig} from '../../shared/application-config/application-config.interface';
+import {PopupService} from '../../shared/popup/popup.service';
 
 @Component({
     selector: 'app-header',
@@ -13,15 +21,16 @@ import {ApplicationConfig} from '../../shared/application-config/application-con
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+    readonly popupService = inject(PopupService);
     readonly config = input.required<ApplicationConfig>();
 
     readonly menuClick = output();
 
-    openPopup(_template: TemplateRef<{$implicit: string}>) {
-        // this.popupService.openPopup(template, context);
+    openPopup(_template: TemplateRef<unknown>) {
+        this.popupService.openPopup(_template, this.config().title);
     }
 
     closePopup() {
-        // this.popupService.closePopup();
+        this.popupService.closePopup();
     }
 }
