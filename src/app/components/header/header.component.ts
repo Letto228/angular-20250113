69899c7f7ecc9c0195eaeb9c9594a546e1c3e33c
Letto228 <letto1101@gmail.com>
2025-02-27 20:03:10 +0300

    feat: add navigate by logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,13 +9,14 @@ import {
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 import {ApplicationConfig} from '../../shared/application-config/application-config.interface';
 import {PopupService} from '../../shared/popup/popup.service';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+    imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
