ef79125dd20b2a0cafb698f7ba2836d0a18e9451
Arthur Buravlev <buravlev-arthur@yandex.ru>
2025-03-02 13:06:11 +0300

    homework 10: review fixes

--- src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -1,4 +1,5 @@
 import {Directive, output, signal} from '@angular/core';
+import {scrollBottomThreshold} from './consts';
 
 @Directive({
     selector: '[appScrollWithLoading]',
@@ -8,21 +9,21 @@ import {Directive, output, signal} from '@angular/core';
     },
 })
 export class ScrollWithLoadingDirective {
-    private readonly scrollBottomThreshold: number = 100;
     private readonly previousScrollTop = signal<number>(0);
 
-    loadNextData = output<void>();
+    readonly loadNextData = output<void>();
 
     onScroll(event: MouseEvent): void {
         const {scrollHeight, scrollTop, clientHeight} = event.target as HTMLElement;
         const scrollBottom = scrollHeight - scrollTop - clientHeight;
-        const isSmallScrollBottomRest = scrollBottom <= this.scrollBottomThreshold;
+        const isSmallScrollBottomRest = scrollBottom <= scrollBottomThreshold;
         const isScrollDirectionDown = scrollTop > this.previousScrollTop();
+
+        this.previousScrollTop.set(scrollTop);
 
         // если для прокрутки вниз осталось меньшее 100px и список скроллится вниз,
         // тогда вызываем событие для подгрузки новых элементов
         if (isSmallScrollBottomRest && isScrollDirectionDown) {
-            this.previousScrollTop.set(scrollTop);
             this.loadNextData.emit();
         }
     }
